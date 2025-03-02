88dd125664e7f59e507f5600deb39cc19c8b8e5e
Mr-Himanshu-Dwivedi <himanshudwivedi2003@gmail.com>
2025-03-02 19:20:15 +0530

    UC2 using MVC

--- src/main/java/com/First/HelloWorld/HelloWorldController.java
+++ src/main/java/com/First/HelloWorld/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.First.HelloWorld;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
-    @GetMapping("/")
-    public String sayHello() {
-        return "Hello from BridgeLabz";
+    @GetMapping("/web")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/web/message")
+    public String message(Model model) {
+        model.addAttribute("message", "This is a custom message, Hello BridgeLabz");
+        return "message";
     }
 }
